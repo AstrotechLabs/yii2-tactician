3483cb10834068eee1e1d23a7ce5f4f0f74b8d3e
Kilderson Sena <dersonsena@gmail.com>
2021-05-27 18:44:27 -0300

    implementation of command as string path

--- src/Yii2TacticianCommandBus.php
+++ src/Yii2TacticianCommandBus.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace DersonSena\Yii2Tactician;
 
 use Yii;
-use DersonSena\Yii2Tactician\Exceptions\MissingHandleMethodException;
+use InvalidArgumentException;
+use RuntimeException;
 use DersonSena\Yii2Tactician\MethodName\Handle;
 use DersonSena\Yii2Tactician\ClassName\Suffix;
 use yii\base\Component;
@@ -30,14 +31,37 @@ final class Yii2TacticianCommandBus extends Component
         $this->commandBus = new CommandBus([$lockingMiddleware, $handlerMiddleware]);
     }
 
-    public function handle(object $commandObject)
+    /**
+     * @param string|object $command Command class object or string path
+     * @param array $parameters (Optional) Parameters to be used when string path is provided
+     * @return false|mixed
+     * @throws \yii\base\InvalidConfigException
+     * @throws \yii\di\NotInstantiableException
+     */
+    public function handle($command, array $parameters = [])
     {
         $callable = [$this->commandBus, 'handle'];
 
-        if (!is_callable($callable)) {
-            throw new MissingHandleMethodException();
+        if (is_string($command)) {
+            if (empty($parameters)) {
+                throw new InvalidArgumentException("You must provide parameters when command is a string path.");
+            }
+
+            /** @var \DersonSena\Yii2Tactician\Handler $handleObject */
+            $handleObject = Yii::$container->get($command);
+            $commandClass = $handleObject->commandClassName();
+
+            if (!class_exists($commandClass)) {
+                throw new RuntimeException("Command class '{$commandClass}' doesn't exists.");
+            }
+
+            $command = $commandClass::create($parameters);
+
+            return call_user_func_array($callable, [$command]);
         }
 
-        return call_user_func_array($callable, [$commandObject]);
+        if (is_object($command) && is_callable($callable)) {
+            return call_user_func_array($callable, [$command]);
+        }
     }
 }
